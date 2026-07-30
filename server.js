07370c4cac309edafa25f366e7223bb679cadd53
shakeef <shakeef2000@gmail.com>
2026-07-30 15:35:31 +0530

    vertion02

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/api/logs', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-print("sucess")
+
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
 
 
